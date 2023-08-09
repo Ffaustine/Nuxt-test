2423e95b2a6e96579caf671e58b7d37cdf230fb6
Faustine Fourcade <faustine.fourcade@hotmail.fr>
2023-08-09 17:21:44 +0200

    button created

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <button id="button" @click='sendMessage'>Send message</button>
+  </div>
 </template>
+
 
 <script>
 import Pusher from 'pusher-js'
 
 export default {
-  name: 'IndexPage'
+  methods: {
+    sendMessage() {
+      console.log("hello")
+
+    }
+  }
 }
+
 </script>
